d312a2f6fe02bb2d41541840915a8e6ad329c5b8
radomirovicvladimir <vladimirradomiroviccv@gmail.com>
2026-07-02 06:26:21 -0700

    .

--- build/src/cpp_runtime.d
+++
@@ -1,6 +0,0 @@
-build/src/cpp_runtime.o: src/cpp_runtime.cpp src/../lib/hw.h \
- src/../h/syscall_c.h
-
-src/../lib/hw.h:
-
-src/../h/syscall_c.h:
--- build/src/syscall.d
+++
@@ -1,8 +0,0 @@
-build/src/syscall.o: src/syscall.cpp src/../h/syscall_c.h \
- src/../h/../lib/hw.h src/../h/syscall_abi.hpp
-
-src/../h/syscall_c.h:
-
-src/../h/../lib/hw.h:
-
-src/../h/syscall_abi.hpp:
